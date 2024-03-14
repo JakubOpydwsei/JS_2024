2f66ec58928f165d3c1178d52d2014046bd6e47c
Jakub Opyd <jakub.opyd@microsoft.wsei.edu.pl>
2024-03-14 01:45:25 +0100

    Lab02

--- JS_2024/Lab_02/script.js
+++ JS_2024/Lab_02/script.js
@@ -4,72 +4,113 @@
 //https://github.com/rbrzegowy/Programowanie-w-Javascript---stacjonarni-2024/tree/main/Lab%202%20-%20slider
 
 
-const img1 = new Image(100,100)
+const img0 = new Image()
+const img1 = new Image()
 const img2 = new Image()
 const img3 = new Image()
 const img4 = new Image()
 const img5 = new Image()
-const img6 = new Image()
 
-img1.src="https://picsum.photos/100"
-img2.src="https://picsum.photos/100"
-img3.src="https://picsum.photos/100"
-img4.src="https://picsum.photos/100"
-img5.src="https://picsum.photos/100"
-img6.src="https://picsum.photos/100"
+img0.src="https://picsum.photos/id/12/200"
+img1.src="https://picsum.photos/id/13/200"
+img2.src="https://picsum.photos/id/14/200"
+img3.src="https://picsum.photos/id/15/200"
+img4.src="https://picsum.photos/id/16/200"
+img5.src="https://picsum.photos/id/17/200"
 
 const package = document.querySelector('.slider')
-package.appendChild(img1)
-package.appendChild(img2)
-package.appendChild(img3)
-package.appendChild(img4)
-package.appendChild(img5)
-package.appendChild(img6)
 
 // pobranie przycisków 
-const zdj1 = document.querySelector('zdj1')
-const zdj2 = document.querySelector('zdj2')
-const zdj3 = document.querySelector('zdj3')
-const zdj4 = document.querySelector('zdj4')
-const zdj5 = document.querySelector('zdj5')
-const zdj6 = document.querySelector('zdj6')
+const zdj0 = document.querySelector('zdj1')
+const zdj1 = document.querySelector('zdj2')
+const zdj2 = document.querySelector('zdj3')
+const zdj3 = document.querySelector('zdj4')
+const zdj4 = document.querySelector('zdj5')
+const zdj5 = document.querySelector('zdj6')
 
 
 
-
+// zdj0.addEventListener(onclick, ******)
 // zdj1.addEventListener(onclick, ******)
 // zdj2.addEventListener(onclick, ******)
 // zdj3.addEventListener(onclick, ******)
 // zdj4.addEventListener(onclick, ******)
 // zdj5.addEventListener(onclick, ******)
-// zdj6.addEventListener(onclick, ******)
 
 
-
-// setTimeout(), setInterval()
-// setTimeout(
-//     ()=>{
-//         console.log('out')
-//         const box = document.querySelector('#slider-inner')
-//         box.style.transform = 'translate(200px ,0px)'
-//     }
-// ,2_000)
 
 let positionX = 0
-let opacity = 1
-const anim = setInterval(
-    ()=>{
-        const box = document.querySelector('#slider-inner')
-        box.style.transform = 'translate( -' +  positionX + 'px ,0px)'
-        box.style.opacity = opacity
-        positionX++
-        opacity = opacity -0.005
-    }
-,16)
 
+function next_slide(which){
+
+//let opacity = 1
+
+ const anim = setInterval(
+     ()=>{
+        const images = document.querySelectorAll('img')
+
+
+        //images[which].style.opacity = opacity    
+        //opacity = opacity -0.025
+        
+        positionX = positionX + 5
+        
+
+        // images[which].style.transform = 'translate( -' + positionX + 'px ,0px)'
+        // images[which + 1].style.transform = 'translate( -' + positionX + 'px ,0px)'
+        // images[which + 2].style.transform = 'translate( -' + positionX + 'px ,0px)'
+        // //images[which + 3].style.opacity = 1
+        // images[which + 3].style.transform = 'translate( -' + positionX + 'px ,0px)'
+        // images[which + 4].style.transform = 'translate( -' + positionX + 'px ,0px)'
+        // images[which + 5].style.transform = 'translate( -' + positionX + 'px ,0px)'
+
+        box.style.transform = 'translate( -' + positionX + 'px ,0px)'
+
+
+        
+        
+
+        },16)
 
 setTimeout( ()=> {
     clearInterval(anim)
-},6_000)
+},760)
 
 
+}
+
+function reset_slide(){
+    const anim = setInterval(
+        ()=>{
+            positionX = positionX - 30
+            box.style.transform = 'translate( -' + positionX + 'px ,0px)'
+        },16)
+    
+}
+
+function start_slide(){
+
+    
+    setInterval( ()=> {
+        next_slide(0)
+    },2_000)
+
+    setTimeout( ()=> {
+        reset_slide()
+    },12_000)    
+
+}
+
+start_slide()
+
+
+const box = document.querySelector('.box')
+
+
+
+box.appendChild(img0)
+box.appendChild(img1)
+box.appendChild(img2)
+box.appendChild(img3)
+box.appendChild(img4)
+box.appendChild(img5)
