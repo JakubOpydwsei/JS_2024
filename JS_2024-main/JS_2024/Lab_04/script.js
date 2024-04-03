d1dbb3523f16317340a7b38096bd4f88f439c7db
Jakub Opyd <jakub.opyd@microsoft.wsei.edu.pl>
2024-04-03 21:42:03 +0200

    dodawanie oraz wyświetlanie

--- JS_2024-main/JS_2024/Lab_04/script.js
+++ JS_2024-main/JS_2024/Lab_04/script.js
@@ -21,18 +21,169 @@
 // Pin (boolean) - możliwość przypięcia do góry na liście notatek
 // Datę utworzenia
 
-// localStorage.setItem('note1_title','first note')
-// localStorage.setItem('note1_content','<ul><li>1</li> <li>2</li> <li>3</li> <li>4</li></ul>')
-// localStorage.setItem('note1_colour','first note')
-// localStorage.setItem('note1_ispin','first note')
-// localStorage.setItem('note1_date','first note')
+localStorage.setItem('note1_title','Notatka startowa ')
+localStorage.setItem('note1_content','Tutaj możesz umieścić swoje notatki')
+localStorage.setItem('note1_colour','green')
+localStorage.setItem('note1_ispin','false')
+let texdate = JSON.stringify(new Date().toLocaleDateString())
+localStorage.setItem('note1_date', texdate.slice(1, texdate.length -1))
+let notes = []
+
+
+
+
+
+
+
+let deleteBtns =document.querySelectorAll('.delete')
+notesGenerate()
+
+
+
+
+function deleteNote(btn) {
+
+   let index = btn.target.id.slice(btn.target.id.length -1)
+
+         localStorage.removeItem('note' + index + '_title')
+         localStorage.removeItem('note' + index + '_content')
+         localStorage.removeItem('note' + index + '_colour')
+         localStorage.removeItem('note' + index + '_ispin')
+         localStorage.removeItem('note' + index + '_date')
+
+         console.log(btn.target.id)
+
+         notesGenerate()
+}
+
+
+function notesGenerate() {
+   document.querySelector('#container').innerHTML = ''
+   notes = []
+   notes_prio = []
+   notes_nonprio = []
+
+   for (let index = 0; index < 50; index++) {
+
+      if (localStorage.getItem('note' + index + '_ispin') == 'true') {
+         notes.push(index)
+         notes_prio.push(index)
+      }
+      if (localStorage.getItem('note' + index + '_ispin') == 'false') {
+         notes.push(index)
+         notes_nonprio.push(index)
+      }
+         // const noteContainer = document.createElement("div") 
+         // const editBtn = document.createElement("button") 
+         // editBtn.innerHTML = 'Edytuj'
+         // editBtn.id = 'editBtn_' + index
+         // const deleteBtn  = document.createElement("button") 
+         // deleteBtn.innerHTML = 'Usuń'
+         // deleteBtn.id = 'deleteBtn_' + index
+         // deleteBtn.classList.add('delete')
+
+         // const note = localStorage.getItem('note' + index + '_title') + " " +
+         //    localStorage.getItem('note' + index + '_date') +
+         //    '<br>' + localStorage.getItem('note' + index + '_content')
+
+         // noteContainer.innerHTML += note
+         // const colour = localStorage.getItem('note' + index + '_colour')
+         // //colour = colour.slice(1, colour.length -1)
+         // noteContainer.style.background = colour
+         // const ids = 'note_' + index
+         // noteContainer.id = ids
+         // document.querySelector('#container').appendChild(noteContainer)
+
+         // document.querySelector('#' + ids).appendChild(editBtn)
+         // document.querySelector('#' + ids).appendChild(deleteBtn)
+
+         
+         // deleteBtns = document.querySelectorAll('.delete')
+
+         // deleteBtns.forEach(element => {
+         //    element.addEventListener('click', deleteNote)
+         // });
+      
+
+   }
+
+   notes_prio.forEach(index => {
+         const noteContainer = document.createElement("div") 
+         const editBtn = document.createElement("button") 
+         editBtn.innerHTML = 'Edytuj'
+         editBtn.id = 'editBtn_' + index
+         const deleteBtn  = document.createElement("button") 
+         deleteBtn.innerHTML = 'Usuń'
+         deleteBtn.id = 'deleteBtn_' + index
+         deleteBtn.classList.add('delete')
+
+         const note = localStorage.getItem('note' + index + '_title') + " " +
+            localStorage.getItem('note' + index + '_date') +
+            '<br>' + localStorage.getItem('note' + index + '_content')
+
+         noteContainer.innerHTML += note
+         const colour = localStorage.getItem('note' + index + '_colour')
+         //colour = colour.slice(1, colour.length -1)
+         noteContainer.style.background = colour
+         const ids = 'note_' + index
+         noteContainer.id = ids
+         document.querySelector('#container').appendChild(noteContainer)
+
+         document.querySelector('#' + ids).appendChild(editBtn)
+         document.querySelector('#' + ids).appendChild(deleteBtn)
+
+         
+         deleteBtns = document.querySelectorAll('.delete')
+
+         deleteBtns.forEach(element => {
+            element.addEventListener('click', deleteNote)
+         });
+   });
+
+   notes_nonprio.forEach(index => {
+      const noteContainer = document.createElement("div") 
+         const editBtn = document.createElement("button") 
+         editBtn.innerHTML = 'Edytuj'
+         editBtn.id = 'editBtn_' + index
+         const deleteBtn  = document.createElement("button") 
+         deleteBtn.innerHTML = 'Usuń'
+         deleteBtn.id = 'deleteBtn_' + index
+         deleteBtn.classList.add('delete')
+
+         const note = localStorage.getItem('note' + index + '_title') + " " +
+            localStorage.getItem('note' + index + '_date') +
+            '<br>' + localStorage.getItem('note' + index + '_content')
+
+         noteContainer.innerHTML += note
+         const colour = localStorage.getItem('note' + index + '_colour')
+         //colour = colour.slice(1, colour.length -1)
+         noteContainer.style.background = colour
+         const ids = 'note_' + index
+         noteContainer.id = ids
+         document.querySelector('#container').appendChild(noteContainer)
+
+         document.querySelector('#' + ids).appendChild(editBtn)
+         document.querySelector('#' + ids).appendChild(deleteBtn)
+
+         
+         deleteBtns = document.querySelectorAll('.delete')
+
+         deleteBtns.forEach(element => {
+            element.addEventListener('click', deleteNote)
+         });
+   });
+
+}
+
+
+
+
 
 //document.querySelector('body').innerHTML = localStorage.getItem('note1_content')
 
 const butonAdd = document.querySelector('#buttonAdd')
 
-//butonAdd.addEventListener('click',addNote)
-//let numbers
+butonAdd.addEventListener('click', addNote)
 
 function addNote() {
    const title = document.querySelector('#title').value
@@ -40,13 +191,34 @@ function addNote() {
    const colour = document.querySelector('#colour').value
    const pin = document.querySelector('#pin').checked
 
-localStorage.setItem('note' + numbersOfNotes +'_title',JSON.stringify(title))
-localStorage.setItem('note' + numbersOfNotes +'_content',JSON.stringify(content))
-localStorage.setItem('note' + numbersOfNotes +'_colour',JSON.stringify(colour))
-localStorage.setItem('note' + numbersOfNotes +'_ispin',JSON.stringify(pin))
-localStorage.setItem('note' + numbersOfNotes +'_date',JSON.stringify(new Date().toISOString()))
+   let numbersOfNotes = 0
 
-//localStorage.setItem('numbersOfNotes','1')
+   for (let index = 1; index <= notes.length + 1; index++) {
+
+      if (localStorage.getItem('note' + index + '_title') == null) {
+         numbersOfNotes = index
+
+         const texttitle = JSON.stringify(title)
+         localStorage.setItem('note' + numbersOfNotes + '_title', texttitle.slice(1, texttitle.length -1))
+
+         const textcontent= JSON.stringify(content)
+         localStorage.setItem('note' + numbersOfNotes + '_content', textcontent.slice(1, textcontent.length -1))
+
+         const textcolour= JSON.stringify(colour)
+         localStorage.setItem('note' + numbersOfNotes + '_colour', textcolour.slice(1, textcolour.length -1))
+         
+         localStorage.setItem('note' + numbersOfNotes + '_ispin', JSON.stringify(pin))
+
+         const texdate = JSON.stringify(new Date().toLocaleDateString())
+         localStorage.setItem('note' + numbersOfNotes + '_date', texdate.slice(1, texdate.length -1))
+
+         break;   
+      }
+
+   }
+   notesGenerate()
+
+   //localStorage.setItem('numbersOfNotes','1')
 }
 
 // for (let index = 0; index < numbersOfNotes; index++) {
@@ -57,4 +229,11 @@ localStorage.setItem('note' + numbersOfNotes +'_date',JSON.stringify(new Date().
 //     document.querySelector('#container').innerHTML = ' Treść notatki' + localStorage.getItem('note' + index +'_date')
 
 // }
+
+
+
+// numbersfNote=1
+// let note = localStorage.getItem('note' + numbersfNote +'_title') + " " + localStorage.getItem('note' + numbersfNote +'_date') +
+//  '<br>' + localStorage.getItem('note' + numbersfNote +'_content')
+// document.querySelector('#container').innerHTML = note
 
